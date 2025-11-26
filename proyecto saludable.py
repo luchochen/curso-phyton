46df3574d28646337f1d54a7deebe944a6a0fe64
luchochen <luchochen3290@gmail.com>
2025-11-26 09:15:41 -0300

    Add files via upload

---
+++ proyecto saludable.py
@@ -0,0 +1,32 @@
+print("--ANALISIS DE SUEÑO--")
+
+print("ingrese fin para terminar")
+
+horas_dormidas=""
+horas =""
+
+while horas_dormidas != "fin" :
+    
+    horas_dormidas=input ("ingrese la cantidad de horas que duerme: ")
+    
+    if horas_dormidas == ("fin"):
+        print("terminado")
+        break
+
+    horas = float(horas_dormidas)
+
+
+    if horas  < 6 :
+        print("dormiste poco")
+    
+
+    elif horas > 8:
+        print("dormiste bastante")
+        
+    
+    else:
+        print("buen descanso")
+        
+
+
+        
